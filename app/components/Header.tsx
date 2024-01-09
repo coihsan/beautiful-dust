f9a1669a97cec166295f68d9ac6843e3cb32a44d
coihsan <110887888+coihsan@users.noreply.github.com>
2024-01-09 04:55:48 +0000

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,15 +4,20 @@ export default function Header() {
   return (
     <header className="max-w-screen-md mx-auto bg-[#17181C] px-6 text-zinc-100 rounded-lg mt-5">
       <div className="w-full mx-auto flex items-center justify-between py-3">
-        <div className="flex items-center gap-5">
-          <Image
-            className="rounded-full border-2"
-            src={"/avatar.webp"}
-            width={40}
-            height={40}
-            alt="logo"
-          />
-          <span className="font-bold text-lg">coihsan</span>
+        <div className="flex items-center">
+          <div className="flex items-center gap-2">
+            <Image
+              className="rounded-full border-2"
+              src={"/avatar.webp"}
+              width={40}
+              height={40}
+              alt="logo"
+            />
+            <span className="font-bold text-lg">coihsan</span>
+          </div>
+          <nav className="ml-5">
+            <a href="">Projects</a>
+          </nav>
         </div>
         <DarkMode />
       </div>
