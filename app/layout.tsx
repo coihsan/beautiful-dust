482d25608e906d354a6c3cf24672cb5379358a1a
coihsan <110887888+coihsan@users.noreply.github.com>
2024-01-11 23:41:09 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "˗ˏˋIhsanˎˊ",
@@ -16,12 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="googlebot" content="noindex" />
+        {/* <GoogleAnalytics gaId="G-GFF36FH35B" /> */}
+      </head>
       <body className="bg-white dark:bg-zinc-950 text-zinc-800 dark:text-zinc-100 max-w-screen-md px-6 mx-auto">
         <Header />
         <main>{children}</main>
         <Footer />
       </body>
-      <GoogleAnalytics gaId="G-GFF36FH35B" />
     </html>
   );
 }
